7d49607ca10d30af036bfeaf41456e547335a8b2
Lizka <Lizka@DESKTOP-3GC8NOB>
2024-03-20 00:13:57 +0500

    Добавьте файлы проекта.

---
+++ ab/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        Console.WriteLine(CountSequences(a, b));
+    }
+
+    public static int CountSequences(int a, int b)
+    {
+        if (a == 0 && b == 0)
+        {
+            return 1;
+        }
+
+        if (a < 0 || b < 0)
+        {
+            return 0;
+        }
+
+        if (a == 0)
+        {
+            return CountSequences(b - 1, 0);
+        }
+
+        if (b == 0)
+        {
+            return CountSequences(a - 1, 0);
+        }
+
+        return CountSequences(a - 1, b) + CountSequences(a, b - 1);
+    }
+}
